8210dff5ffef1c34711c76aa87840263555b71ce
jdragon <ldap01@naver.com>
2020-09-17 23:30:48 +0900

    MariaDB관련자원_200917

--- Investar/DBUpdater.py
+++ Investar/DBUpdater.py
@@ -1,7 +1,40 @@
 # DBUpdater - 스텁(stub)코드 : 인터페이스는 정의되어 있으나 실제 코드가 구현되지 않은 상태의 코드
+import pymysql
+
+
 class DBUpdater:
     def __init__(self):
         """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root', password='admin',
+                                    db='inverstar', charset='utf8')
+        with self.conn.cursor() as curs:
+            sql = """
+            CREATE TABLE if NOT EXISTS company_info (
+                code VARCHAR(20),
+                company VARCHAR(40),
+                last_update DATE,
+                PRIMARY KEY (CODE)
+            )
+            """
+            curs.execute(sql)
+            sql = """
+            CREATE TABLE if NOT EXISTS daily_price (
+                code VARCHAR(20),
+                DATE DATE,
+                OPEN BIGINT(20),
+                high BIGINT(20),
+                low BIGINT(20),
+                close BIGINT(20),
+                diff BIGINT(20),
+                volume BIGINT(20),
+                PRIMARY KEY (CODE, DATE)
+            )
+            """
+            curs.execute(sql)
+        self.conn.commit()
+
+        self.codes = dict()
+        self.update_comp_info()
         
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
---
+++ query/ch05_04_createtable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE if NOT EXISTS company_info (
+	code VARCHAR(20),
+	company VARCHAR(40),
+	last_update DATE,
+	PRIMARY KEY (CODE)
+);
+
+CREATE TABLE if NOT EXISTS daily_price (
+	code VARCHAR(20),
+	DATE DATE,
+	OPEN BIGINT(20),
+	high BIGINT(20),
+	low BIGINT(20),
+	close BIGINT(20),
+	diff BIGINT(20),
+	volume BIGINT(20),
+	PRIMARY KEY (CODE, DATE)
+);
